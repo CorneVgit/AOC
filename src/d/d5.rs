ca67625fa6d6ac362c0079939781f244706dcd16
CorneVgit <corneverduin@hotmail.com>
2021-12-06 01:20:05 +0100

    refactor

--- src/d/d5.rs
+++ src/d/d5.rs
@@ -57,6 +57,7 @@ fn d5values() -> Vec<Line> {
 
 pub fn d5a() {
     let lines = d5values();
+
     let mut non_diagonal_lines: Vec<Line> = Vec::new();
     let mut diagonal_lines: Vec<Line> = Vec::new();
 
@@ -73,14 +74,14 @@ pub fn d5a() {
     for line in &non_diagonal_lines {
         for x in line.from.x..=line.to.x {
             for y in line.from.y..=line.to.y {
-                let p = Point { x, y };
+                let point = Point { x, y };
 
-                match field.entry(p) {
-                    Occupied(o) => *o.into_mut() += 1,
-                    Vacant(_) => {
-                        field.insert(p, 1);
-                    }
+                let field_point = match field.entry(point) {
+                    Occupied(entry) => entry.into_mut(),
+                    Vacant(entry) => entry.insert(0),
                 };
+
+                *field_point += 1;
             }
         }
     }
@@ -90,6 +91,7 @@ pub fn d5a() {
 
 pub fn d5b() {
     let lines = d5values();
+
     let mut non_diagonal_lines: Vec<Line> = Vec::new();
     let mut diagonal_lines: Vec<Line> = Vec::new();
 
@@ -106,33 +108,33 @@ pub fn d5b() {
     for line in &non_diagonal_lines {
         for x in line.from.x..=line.to.x {
             for y in line.from.y..=line.to.y {
-                let p = Point { x, y };
+                let point = Point { x, y };
 
-                match field.entry(p) {
-                    Occupied(o) => *o.into_mut() += 1,
-                    Vacant(_) => {
-                        field.insert(p, 1);
-                    }
+                let field_point = match field.entry(point) {
+                    Occupied(entry) => entry.into_mut(),
+                    Vacant(entry) => entry.insert(0),
                 };
+
+                *field_point += 1;
             }
         }
     }
 
     for line in &diagonal_lines {
         for n in 0..=(line.to.x - line.from.x) {
-            let mut p: Point = Point::new();
-            p.x = line.from.x + n;
+            let mut point: Point = Point::new();
+            point.x = line.from.x + n;
             match line.from.y > line.to.y {
-                true => p.y = line.from.y - n,
-                false => p.y = line.from.y + n,
+                true => point.y = line.from.y - n,
+                false => point.y = line.from.y + n,
             }
 
-            match field.entry(p) {
-                Occupied(o) => *o.into_mut() += 1,
-                Vacant(_) => {
-                    field.insert(p, 1);
-                }
+            let field_point = match field.entry(point) {
+                Occupied(entry) => entry.into_mut(),
+                Vacant(entry) => entry.insert(0),
             };
+
+            *field_point += 1;
         }
     }
 
