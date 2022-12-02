7f5764f2b832b22e1778b081471785c59f3f2515
Corné Verduin <c.verduin@hgg-group.com>
2022-12-02 13:56:19 +0100

    2022 day 2

--- 2022/benches/d.rs
+++ 2022/benches/d.rs
@@ -1,9 +1,10 @@
 use criterion::{criterion_group, criterion_main, Criterion};
-use aoc2022::d::d1::*;
+use aoc2022::d::*;
 
 
 fn criterion_benchmark(c: &mut Criterion) {
-    c.bench_function("d1", |b| b.iter(d1));
+    c.bench_function("d1", |b| b.iter(d1::d1));
+    c.bench_function("d2", |b| b.iter(d2::d2));
 }
 
 criterion_group!(benches, criterion_benchmark);
---
+++ 2022/src/d/d2.rs
@@ -0,0 +1,63 @@
+use unwrap_infallible::UnwrapInfallible;
+
+use crate::util::read_all;
+
+fn values() -> Vec<String> {
+    let result_values = read_all::<String>("input_2");
+    let mut games: Vec<String> = Vec::new();
+
+    for result_value in result_values {
+        let game =  result_value.unwrap_infallible();
+        games.push(game);
+    }
+
+    games
+}
+
+pub fn d2() -> (u32, u32) {
+    let games = values();
+
+    let mut score = 0;
+    for game in &games {
+        score += match game.as_str() {
+            // Loss
+            "B X" => 1,
+            "C Y" => 2,
+            "A Z" => 3,
+            // Tie
+            "A X" => 4,
+            "B Y" => 5,
+            "C Z" => 6,
+            // Win
+            "C X" => 7,
+            "A Y" => 8,
+            "B Z" => 9,
+            _ => panic!("Invalid input")
+        }
+    }
+
+    let r1 = score;
+
+    let mut score = 0;
+    for game in &games {
+        score += match game.as_str() {
+            // Loss
+            "A X" => 3,
+            "B X" => 1,
+            "C X" => 2,
+            // Tie
+            "A Y" => 4,
+            "B Y" => 5,
+            "C Y" => 6,
+            // Win
+            "A Z" => 8,
+            "B Z" => 9,
+            "C Z" => 7,
+            _ => panic!("Invalid input")
+        }
+    }
+
+    let r2 = score;
+
+    (r1, r2)
+}
