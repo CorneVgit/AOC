ac64901cd984a41f33528ca764a41e3d9d53dc5c
CorneVgit <corneverduin@hotmail.com>
2022-12-04 16:10:37 +0100

    2022 day 4

--- 2022/benches/d.rs
+++ 2022/benches/d.rs
@@ -5,6 +5,7 @@ fn criterion_benchmark(c: &mut Criterion) {
     c.bench_function("d1", |b| b.iter(d1::d1));
     c.bench_function("d2", |b| b.iter(d2::d2));
     c.bench_function("d3", |b| b.iter(d3::d3));
+    c.bench_function("d4", |b| b.iter(d4::d4));
 }
 
 criterion_group!(benches, criterion_benchmark);
---
+++ 2022/src/d/d4.rs
@@ -0,0 +1,38 @@
+use unwrap_infallible::UnwrapInfallible;
+
+use crate::util::read_all;
+
+fn values() -> Vec<String> {
+    read_all::<String>("input_4")
+        .into_iter()
+        .map(|rucksack| rucksack.unwrap_infallible())
+        .collect()
+}
+
+pub fn d4() -> (u32, u32) {
+    let pairs = values();
+
+    let mut r1 = 0;
+    let mut r2 = 0;
+
+    for pair in pairs {
+        let (e1, e2) = pair.split_once(',').unwrap();
+        let (e1_lb, e1_ub) = e1.split_once('-').unwrap();
+        let (e2_lb, e2_ub) = e2.split_once('-').unwrap();
+
+        let e1_lb: i32 = e1_lb.parse().unwrap(); // elf 1 lower bound
+        let e1_ub: i32 = e1_ub.parse().unwrap(); // elf 1 upper bound
+        let e2_lb: i32 = e2_lb.parse().unwrap(); // elf 2 lower bound
+        let e2_ub: i32 = e2_ub.parse().unwrap(); // elf 2 upper bound
+
+        if e1_lb <= e2_lb && e1_ub >= e2_ub || e1_lb >= e2_lb && e1_ub <= e2_ub {
+            r1 += 1;
+        }
+
+        if e1_lb <= e2_lb && e1_ub >= e2_lb || e1_lb >= e2_lb && e1_lb <= e2_ub {
+            r2 += 1;
+        }
+    }
+
+    (r1, r2)
+}
