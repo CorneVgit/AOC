bc97864ae9572829e28a2b01fee96c0fd01f57e0
CorneVgit <corneverduin@hotmail.com>
2023-12-02 17:05:02 +0100

    2023 day 2

---
+++ 2023/src/bin/d2.rs
@@ -0,0 +1,7 @@
+use aoc2023::d::d2::*;
+
+fn main() {
+    let (r1, r2) = d2();
+    println!("{r1}");
+    println!("{r2}");
+}
---
+++ 2023/src/d/d2.rs
@@ -0,0 +1,85 @@
+use itertools::Itertools;
+use unwrap_infallible::UnwrapInfallible;
+
+use crate::util::read_all;
+
+fn get_games() -> Vec<String> {
+    let result_values = read_all::<String>("input_2");
+
+    result_values
+        .into_iter()
+        .map(|result_values| result_values.unwrap_infallible())
+        .collect()
+}
+
+pub fn d2() -> (i32, i32) {
+    const BAG_RED: i32 = 12;
+    const BAG_GREEN: i32 = 13;
+    const BAG_BLUE: i32 = 14;
+
+    let games = get_games();
+
+    let mut r1 = 0;
+
+    'games: for game in &games {
+        let (id, sets) = game.split_once(": ").unwrap();
+        let id = id
+            .split_ascii_whitespace()
+            .nth(1)
+            .unwrap()
+            .parse::<i32>()
+            .unwrap();
+        let sets = sets.replace(';', ",");
+
+        for cubes in sets.split(", ").collect_vec() {
+            let n = cubes
+                .split_ascii_whitespace()
+                .next()
+                .unwrap()
+                .parse::<i32>()
+                .unwrap();
+
+            if cubes.contains("red") && n > BAG_RED
+                || cubes.contains("green") && n > BAG_GREEN
+                || cubes.contains("blue") && n > BAG_BLUE
+            {
+                continue 'games;
+            }
+        }
+
+        r1 += id;
+    }
+
+    let mut r2 = 0;
+
+    for game in &games {
+        let sets = game.split_once(": ").unwrap().1;
+
+        let mut red = i32::MIN;
+        let mut green = i32::MIN;
+        let mut blue = i32::MIN;
+
+        let sets = sets.replace(';', ",");
+
+        for cubes in sets.split(", ").collect_vec() {
+            let n = cubes
+                .split_ascii_whitespace()
+                .next()
+                .unwrap()
+                .parse::<i32>()
+                .unwrap();
+
+            if cubes.contains("red") && n > red {
+                red = n;
+            } else if cubes.contains("green") && n > green {
+                green = n;
+            } else if cubes.contains("blue") && n > blue {
+                blue = n;
+            }
+        }
+
+        r2 += red * green * blue;
+    }
+
+    (r1, r2)
+}
