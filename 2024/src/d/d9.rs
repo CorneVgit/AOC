e3b8a6a4c172280d2eebe90928bb63ebc3b03348
Corné Verduin <c.verduin@hgg-group.com>
2024-12-11 16:42:33 +0100

    2024 day 9

--- 2024/src/d/d9.rs
+++ 2024/src/d/d9.rs
@@ -1,29 +1,26 @@
 use crate::util::read_single_string;
 
-#[derive(Debug, PartialEq, Eq, Clone, Copy)]
+#[derive(Default, Copy, Clone)]
 struct Entry {
     id: Option<usize>,
     len: usize,
 }
 
-#[must_use]
-pub fn d9() -> (usize, usize) {
-    let input = read_single_string("input_9_sample");
-
-    let mut entries: Vec<Entry> = Vec::new();
+fn get_input(input: String) -> Vec<Entry> {
+    let mut filesystem: Vec<Entry> = Vec::new();
 
     let mut input_iter = input.trim().chars();
     let mut i = 0;
 
     while let Some(c) = input_iter.next() {
-        entries.push(Entry {
+        filesystem.push(Entry {
             id: Some(i),
             len: c.to_digit(10).unwrap() as usize,
         });
 
         match input_iter.next() {
             Some(c) => {
-                entries.push(Entry {
+                filesystem.push(Entry {
                     id: None,
                     len: c.to_digit(10).unwrap() as usize,
                 });
@@ -34,80 +31,147 @@ pub fn d9() -> (usize, usize) {
         i += 1;
     }
 
-    let mut compacted: Vec<Entry> = Vec::new();
+    filesystem
+}
 
-    let mut f_iter = entries.iter();
-    let mut b = &Entry { id: None, len: 0 };
-    let mut f_left = 0;
-    let mut b_used = 0;
-    'l: while let Some(f) = f_iter.next() {
-        if f == b {
-            break;
-        }
+#[must_use]
+pub fn d9() -> (usize, usize) {
+    let input = read_single_string("input_9");
 
-        if f.id.is_some() {
-            compacted.push(*f);
+    let filesystem = get_input(input);
+
+    (
+        calc_checksum(&compact_filesystem_fragmented(&filesystem)),
+        calc_checksum(&compact_filesystem_whole(&filesystem)),
+    )
+}
+
+fn compact_filesystem_fragmented(filesystem: &[Entry]) -> Vec<Entry> {
+    let mut compacted = Vec::new();
+
+    let mut src = &Entry::default();
+    let mut dest_free_space_left;
+    let mut src_file_parts_left = src.len;
+
+    let mut filesystem_iter = filesystem.iter();
+    while let Some(entry) = filesystem_iter.next() {
+        if entry.id.is_some() {
+            // Entry is a file
+            compacted.push(*entry);
+
             continue;
         }
 
-        f_left = f.len;
-
-        loop {
-            if b_used >= b.len {
-                b = match f_iter.next_back() {
+        // Entry is free space
+        dest_free_space_left = entry.len;
+        while dest_free_space_left > 0 {
+            if src.id.is_none() || src_file_parts_left == 0 {
+                // Find next file to move
+                src = match filesystem_iter.next_back() {
                     Some(e) => e,
-                    None => break 'l,
+                    None => {
+                        break;
+                    }
                 };
+                src_file_parts_left = src.len;
+
+                continue;
             }
 
-            println!("{f:?}, {b:?}, {f_left}, {b_used}");
-            println!("{compacted:?}");
+            match dest_free_space_left.cmp(&src_file_parts_left) {
+                std::cmp::Ordering::Less => {
+                    compacted.push(Entry {
+                        id: src.id,
+                        len: dest_free_space_left,
+                    });
 
-            if b.id.is_some() {
-                match f_left.cmp(&(b.len - b_used)) {
-                    std::cmp::Ordering::Less => {
-                        compacted.push(Entry {
-                            id: b.id,
-                            len: f_left,
-                        });
-                        b_used += f_left;
-                        break;
-                    }
-                    std::cmp::Ordering::Equal => {
-                        compacted.push(Entry {
-                            id: b.id,
-                            len: f.len,
-                        });
-                        b_used = b.len;
-                        break;
-                    }
-                    std::cmp::Ordering::Greater => {
-                        compacted.push(Entry {
-                            id: b.id,
-                            len: f_left,
-                        });
-                        f_left -= b.len;
-                        b_used = b.len;
-                        continue;
-                    }
+                    // Moved part of the file to fill the whole empty space
+                    src_file_parts_left -= dest_free_space_left;
+
+                    break;
                 }
-            } else {
-                b_used = b.len
+                std::cmp::Ordering::Equal => {
+                    compacted.push(Entry {
+                        id: src.id,
+                        len: dest_free_space_left,
+                    });
+
+                    // Moved the whole file to fill the whole empty space
+                    src_file_parts_left = 0;
+
+                    break;
+                }
+                std::cmp::Ordering::Greater => {
+                    compacted.push(Entry {
+                        id: src.id,
+                        len: src_file_parts_left,
+                    });
+
+                    // Moved the whole file to fill part of the empty space
+                    dest_free_space_left -= src_file_parts_left;
+                    src_file_parts_left = 0;
+                }
             }
         }
     }
 
-    println!("{compacted:?}");
+    if let Some(e) = compacted.last_mut() {
+        if src_file_parts_left > 0 && e.id == src.id {
+            e.len += src_file_parts_left;
+        }
+    }
 
-    let mut r1 = 0;
+    compacted
+}
+
+fn compact_filesystem_whole(filesystem: &[Entry]) -> Vec<Entry> {
+    let mut compacted = filesystem.to_vec();
+
+    for entry in filesystem.iter().rev().filter(|e| e.id.is_some()) {
+        if let Some(dest_i) = compacted
+            .iter()
+            .position(|e| e.id.is_none() && e.len >= entry.len)
+        {
+            match compacted.iter().position(|e| e.id == entry.id) {
+                Some(src_i) if dest_i > src_i => continue,
+                Some(src_i) => compacted[src_i].id = None,
+                None => break,
+            }
+
+            if compacted[dest_i].len > entry.len {
+                compacted.insert(
+                    dest_i + 1,
+                    Entry {
+                        id: None,
+                        len: compacted[dest_i].len - entry.len,
+                    },
+                );
+            }
+
+            compacted[dest_i] = *entry;
+        }
+    }
+
+    compacted
+}
+
+fn calc_checksum(compacted: &[Entry]) -> usize {
+    let mut r = 0;
     let mut i = 0;
 
     for f in compacted {
-        for _ in 0..f.len {
-            r1 += i * f.id.unwrap();
-            i += 1;
-        }
+        match f.id {
+            Some(id) => {
+                for _ in 0..f.len {
+                    r += i * id;
+                    i += 1;
+                }
+            }
+            None => {
+                i += f.len;
+            }
+        };
     }
 
-    (r1, 0)
+    r
 }
